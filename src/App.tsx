5ebe8a67ff230d5060704664a2f816f8dd91cb02
Iorgen Wildrik <iorgenwildriksouzasilva@Iorgens-MacBook-Air.local>
2022-02-25 14:47:45 -0300

    feat: add countdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,29 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+
+const COUNTDOWN_INITIAL_TIME_IN_SECONDS = 25 * 60
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [secondsAmount, setSecondsAmount] = useState(COUNTDOWN_INITIAL_TIME_IN_SECONDS)
+
+  useEffect(() => {
+    if(secondsAmount === 0) {
+      alert('Finish')
+      return
+    }
+
+    setTimeout(() => {
+      setSecondsAmount(state => state - 1)
+    }, 1000)
+  }, [secondsAmount])
+
+  const seconds = Math.floor(secondsAmount / 60)
+  const minutes = secondsAmount % 60
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <span>{String(seconds).padStart(2, '0')}</span>
+      <span>:</span>
+      <span>{String(minutes).padStart(2, '0')}</span>
     </div>
   )
 }
